52c019f63d8e3e5ab46391f60e8fbfd6fd3cf909
Smack <kovacsmyles1@gmail.com>
2021-01-24 16:13:34 -0600

    progress on errors preventing display

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,7 +1,8 @@
-var searchBox = document.querySelector("#search-box")
+var searchTerm = document.querySelector("#search-box");
 var cityInputEl = document.querySelector("#city");
-var weatherContainerEl = document.querySelector("#weather-container")
-var weatherSearchTerm = document.querySelector("#city-search-term")
+var weatherContainerEl = document.querySelector("#weather-container");
+var weatherSearchTerm = document.querySelector("#city-search-term");
+var historyContainerEl = document.querySelector("#history-container")
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -13,16 +14,25 @@ var formSubmitHandler = function(event) {
         alert("Please enter a valid City")
     }
     console.log(event);
+    debugger;
 };
+var addToHistory = function() {
+    var historyDisplayEl = document.createElement("<div>");
+    historyDisplayEl.classlist = "list-container";
 
+    var cityHistoryEl = document.createElement("a");
+    cityHistoryEl.classList = "align-center list-item"
+    cityHistoryEl.setAttribute("href", localStorage.getItem("city", []))
+    
+};
 var getWeather = function (city) {
-    var apiUrl = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=863fa8c9dd8dd40d83d3a86ebf49c433";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=863fa8c9dd8dd40d83d3a86ebf49c433";
 
     fetch(apiUrl)
         .then(function(response) {
             if (response.ok) {
                 response.json().then(function(data) {
-                    displayWeather(data.items);
+                    displayWeather(data.weather);
                 });
             } else {
                 alert("Error " + response.statusText);
@@ -31,6 +41,7 @@ var getWeather = function (city) {
         .catch(function(error) {
             alert("Unable to connect to Weather");
         });
+    localStorage.setItem("city", JSON.stringify(apiUrl));   
 };
 
 var displayWeather = function (weather, searchTerm) {
@@ -42,20 +53,30 @@ var displayWeather = function (weather, searchTerm) {
     weatherEl.classList = "list-container";
 
     //create temp element 
-    var tempEl = document.createElement("<p>");
-    tempEl.classList = "temperature";
+    var tempEl = document.createElement("<p>") 
+    tempEl.classList = "temperature list-item";
 
     //create humidity element
     var humidEl = document.createElement("<p>");
-    humidEl.classList= "humidity"
+    humidEl.classList= "humidity list-item"
 
     //create wind element
     var windEl = document.createElement("<p>");
-    windEl.classList= "wind"
+    windEl.classList= "wind list-item"
 
     //create UV element
-    var uvEl = document.createElement("<p>");
-    uvEl.classList= "uv"
+    var uvEl = document.createElement("<p>"); {
+        if (uv < 2) {
+            classList="bg-success"
+        } 
+        if (uv < 5) {
+            classList="bg-warning"
+        }
+        if (uv > 5) (
+            classList="bg-danger"
+        )
+    }
+    uvEl.classList= "uv list-item"
 
     //append to the container
     weatherEl.appendChild(tempEl);
@@ -65,5 +86,5 @@ var displayWeather = function (weather, searchTerm) {
 
     //append container to dom
     weatherContainerEl.appendChild(weatherEl);
-}
-searchBox.addEventListener("submit", formSubmitHandler);
+};
+searchTerm.addEventListener("submit", formSubmitHandler);
